6613bdceebc70dbf3eb90f09e413817ac3bf4002
thynash <24mc3033@rgipt.ac.in>
2025-08-05 16:23:28 +0000

    Streamlit version

--- app.py
+++ app.py
@@ -1,27 +1,30 @@
 import streamlit as st
-from PIL import Image
-import os
+import matplotlib.pyplot as plt
 
-# Directory where fractal images are saved
-IMAGE_DIR = 'Fractal_Shapes'
+# Import your custom modules as regular modules thanks to __init__.py
+from Fractal_Shapes_Generator import Mandelbrot, Julia
 
-# Load images
-def load_image(name):
-    return Image.open(os.path.join(IMAGE_DIR, name))
-
-# Map fractal types to filenames
-fractals = {
-    'Mandelbrot Set': 'Mandelbrot.png',
-    'Julia Set': 'Julia.png',
-#    'Koch Snowflake': 'koch_snowflake_order_4.png',
-#    'Sierpinski Triangle': 'sierpinski_triangle_order_6.png'
-}
-
-# Streamlit UI
 st.title('FractalNotebook Viewer')
 
-selection = st.selectbox('Select Fractal Geometry:', list(fractals.keys()))
+selection = st.sidebar.selectbox('Choose Fractal Geometry:', ['Mandelbrot Set', 'Julia Set'])
 
-if selection:
-    st.image(load_image(fractals[selection]), caption=selection, use_container_width=True)
+if selection == 'Mandelbrot Set':
+    st.markdown(Mandelbrot.mandelbrot_description)
+    max_iter = st.slider('Iterations', 50, 1000, 250, 50)
+    mandelbrot_img = Mandelbrot.generate_mandelbrot_set(-2.0, 1.0, -1.5, 1.5, 500, 350, max_iter)
+    fig, ax = plt.subplots()
+    ax.imshow(mandelbrot_img, cmap='hot', extent=(-2, 1, -1.5, 1.5))
+    ax.axis('off')
+    st.pyplot(fig)
+
+elif selection == 'Julia Set':
+    st.markdown(Julia.julia_description)
+    c_real = st.slider('Real part of c', -1.5, 1.5, -0.8, 0.01)
+    c_imag = st.slider('Imaginary part of c', -1.5, 1.5, 0.156, 0.01)
+    max_iter = st.slider('Iterations', 50, 1000, 250, 50)
+    julia_img = Julia.generate_julia_set(complex(c_real, c_imag), -2.0, 2.0, -2.0, 2.0, 500, 500, max_iter)
+    fig, ax = plt.subplots()
+    ax.imshow(julia_img, cmap='cool', extent=(-2, 2, -2, 2))
+    ax.axis('off')
+    st.pyplot(fig)
 
